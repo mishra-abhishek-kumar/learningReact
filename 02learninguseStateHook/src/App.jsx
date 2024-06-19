c35ea546c959cb000de6996d44aec9e1904abb75
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-06-19 13:26:01 +0530

    problem solved using useState

--- 02learninguseStateHook/src/App.jsx
+++ 02learninguseStateHook/src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
 
-    let count = 10;
+    let [count, setCount] = useState(10);
+
+    // let count = 10;
 
     const increaseCount = () => {
         count = count + 1;
+        setCount(count);
         console.log("Count increased ", count);
     }
 
     const decreaseCount = () => {
         count = count - 1;
+        setCount(count);
         console.log("Count decreased ", count);
     }
 
