3c1f75e72ff625b6f9fb11578c88786df1e09157
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-05 00:18:57 +0530

    another way of creating routes

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Layout'
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "",
-                element: <Home />
-            },
-            {
-                path: "about",
-                element: <About />
-            },
-            {
-                path: "contact",
-                element: <Contact />
-            }
-        ]
-    }
-])
+// //One way of creating routes
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Layout />,
+//         children: [
+//             {
+//                 path: "",
+//                 element: <Home />
+//             },
+//             {
+//                 path: "about",
+//                 element: <About />
+//             },
+//             {
+//                 path: "contact",
+//                 element: <Contact />
+//             }
+//         ]
+//     }
+// ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+//Another way of creating routes
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<Layout />}>
+			<Route path="" element={<Home />} />
+			<Route path="about" element={<About />} />
+			<Route path="contact" element={<Contact />} />
+		</Route>
+	)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
