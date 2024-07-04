171ca1153b87eb1cecab532216b0151f9e0e0209
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-04 15:50:21 +0530

    app completed

--- 06currencyConvertor/src/customHooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/customHooks/useCurrencyInfo.js
@@ -9,8 +9,10 @@ function useCurrencyInfo(currency) {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
+        console.log("data", data);
     }, [currency]);
-
+    console.log(data);
+    return data;
 }
 
 export default useCurrencyInfo;
