ff3a01ad1dcfdb0c802aeb81c70abff49c18d963
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-05 17:48:45 +0530

    sending data from component to context

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
         <h1>Hello</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
