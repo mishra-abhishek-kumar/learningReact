ce1c2883cf8664d5a4c1bbb9294e0731b954cc24
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-06 14:04:02 +0530

    addes components and used TodoContext

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
 import { useEffect } from "react";
+import { TodoForm } from "./components";
+import { TodoItem } from "./components";
 
 function App() {
 	const [todos, setTodos] = useState([]);
@@ -12,9 +14,7 @@ function App() {
 
 	const updateTodo = (id, todo) => {
 		setTodos((prev) =>
-			prev.map((prevTodo) => {
-				prevTodo.id === id ? todo : prevTodo;
-			})
+			prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
 		);
 	};
 
@@ -23,6 +23,7 @@ function App() {
 	};
 
 	const toggleTodo = (id) => {
+		//console.log(id);
 		setTodos((prev) =>
 			prev.map((prevTodo) =>
 				prevTodo.id === id
@@ -53,9 +54,16 @@ function App() {
 					<h1 className="text-2xl font-bold text-center mb-8 mt-2">
 						Manage Your Todos
 					</h1>
-					<div className="mb-4">{/* Todo form goes here */}</div>
+					<div className="mb-4">
+						<TodoForm />
+					</div>
 					<div className="flex flex-wrap gap-y-3">
 						{/*Loop and Add TodoItem here */}
+						{todos.map((todo) => (
+							<div key={todo.id} className="w-full">
+								<TodoItem todo={todo} />
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
