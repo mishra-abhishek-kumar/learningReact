a59972744bcc60f335132b86a6f051c54d185de5
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-07 11:40:25 +0530

    created addTodo reducer

--- 12learningreducToolkit/src/features/todo/todoSlice.js
+++ 12learningreducToolkit/src/features/todo/todoSlice.js
@@ -11,7 +11,20 @@ const initialState = {
 //2nd we provide the initial state
 //3rd we create our reducers
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {}
+	name: "todo",
+	initialState,
+	reducers: {
+		addTodo: (state, action) => {
+            // creating new toto
+            // taking id from nanoID & text from the action which will be sent from useDispatch
+			const todo = {
+				id: nanoid(),
+				text: action.payload,
+			};
+
+            //after creating the todo above, changing the state i.e. adding the todo to the array
+			state.todos.push(todo);
+		},
+		removeTodo: (state, action) => {},
+	},
 });
