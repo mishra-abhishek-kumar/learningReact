6de0d8893ad08c9cf2d4740859830a1ae2d75bc2
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-07 10:58:46 +0530

    accessing same state in diff component using store

--- 11reduxtoolkit/src/App.jsx
+++ 11reduxtoolkit/src/App.jsx
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Home from "./components/Home";
 
 function App() {
+    const c = useSelectorector(state => state.custom.c)
 	return (
 		<>
 			<h1>Hello</h1>
 			<Home />
+            <h2>{c}</h2>
 		</>
 	);
 }
