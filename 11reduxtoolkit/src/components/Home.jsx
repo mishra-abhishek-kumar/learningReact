d10cc540e26d1cd0459a1f2830e3a99a8f038f45
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-07 10:52:59 +0530

    triggering reducers using (action)useDispatch()

--- 11reduxtoolkit/src/components/Home.jsx
+++ 11reduxtoolkit/src/components/Home.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 function Home() {
 	const value = 20;
+	const dispatch = useDispatch();
+
+	const addBtn = () => {
+		dispatch({
+			type: "Increment",
+		});
+	};
+
+	const incrementByVal = () => {
+		dispatch({
+			type: "IncrementByValue",
+			payload: 25,
+		});
+	};
+
+	const subBtn = () => {
+		dispatch({
+			type: "Decrement",
+		});
+	};
 	return (
 		<>
 			<h2>{value}</h2>
 
-			<button>Increment</button>
-            <button>IncrementByValue</button>
-			<button>Decrement</button>
+			<button onClick={addBtn}>Increment</button>
+			<button onClick={incrementByVal}>IncrementByValue</button>
+			<button onClick={subBtn}>Decrement</button>
 		</>
 	);
 }
