494d547cf42e078b0d8ee6c0db4c75c7d635e1b5
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-21 11:47:39 +0530

    added tailwind and wrapped the main.jsx with store provider

--- 13megaBlog/src/App.jsx
+++ 13megaBlog/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from 'react-router-dom'
 
 function App() {
 	const [loading, setLoading] = useState(true);
@@ -21,8 +23,15 @@ function App() {
 			.finally(() => setLoading(false));
 	}, []);
 	return !loading ? (
-        <>
-        </>
+        <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+            <div className="w-full block">
+                <Header />
+                <main>
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+        </div>
     ) : null
 }
 
--- 13megaBlog/src/store/store.js
+++ 13megaBlog/src/store/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 
 //we can give multiple  reducers here so that store have access of all the reducers
-export const store = configureStore({
-    reducer: {}
-})
+const store = configureStore({
+	reducer: authReducer,
+});
+
+export default store;
