ac57eaa5a9d05fd7bac55ba6d0fdbd2ac90da070
Abhishek Kumar Mishra <mishraabhishek0807@gmail.com>
2024-07-21 11:23:35 +0530

    fetching user login status from store to APP.js either to show loader or show some content

--- 13megaBlog/src/App.jsx
+++ 13megaBlog/src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css';
-import config from './config/config';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-    console.log(import.meta.env.VITE_APPWRITE_URL);
-    console.log("working", config.appwriteURL);
-  return (
-    <>
-      <h1>A blog application with AppWrite</h1>
-    </>
-  )
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoader(false));
+	}, []);
+	return !loading ? (
+        <>
+        </>
+    ) : null
 }
 
-export default App
+export default App;
